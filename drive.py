5ad63435fe6e272bc80aad56f536e4b7332a5f6e
Zeng-WCh <lewis.zeng02@gmail.com>
2024-02-23 20:04:25 +0800

    Bug fixed

--- drive.py
+++ drive.py
@@ -6,6 +6,8 @@ import sys
 import json
 import os
 
+DEBUG = os.environ.get('DEBUG', False)
+
 def init_json(input_json: str):
     with open(input_json, 'r') as f:
         data = json.load(f)
@@ -47,46 +49,39 @@ def main() -> int:
         return 1
     filename = 'tmp.json'
     base = init_json(sys.argv[2])
-    # Create some regular expressions
+    # Create some regular expressions used by perf
     # Cache-Miss
-    WORST_CM = 0
-    CM = re.compile(r'.*(\d+).*(cache-misses).*')
-    # L1-dcache-load-misses
-    WORST_L1DLM = 0
-    L1DLM = re.compile(r'.*(\d+).*(L1-dcache-load-misses).*')
-    # L1-icache-load-misses
-    WORST_L1ILM = 0
-    L1ILM = re.compile(r'.*(\d+).*(L1-icache-load-misses).*')
-    # Cache-Reference
-    WORST_CR = 0
-    CR = re.compile(r'.*(\d+).*(cache-references).*')
-    # Bus-Cycle
-    WORST_BC = 0
-    BC = re.compile(r'.*(\d+).*(bus-cycles).*')
-    # L1-dcache-loads
-    WORST_L1DL = 0
-    L1DL = re.compile(r'.*(\d+).*(L1-dcache-loads).*')
-    # L1-icache-loads
-    WORST_L1IL = 0
-    L1IL = re.compile(r'.*(\d+).*(L1-icache-loads).*')
-    # L1-dcache-store-misses
-    WORST_L1DSM = 0
-    L1DSM = re.compile(r'.*(\d+).*(L1-dcache-store-misses).*')
-    # bus-access, but only for ARM-A72
-    WORST_BA = 0
-    BA = re.compile(r'.*(\d+).*(armv8_cortex_a72/bus_access/).*')
+    # CM = re.compile(r'\s*(\d+)\s*(cache-misses).*')
+    # # L1-dcache-load-misses
+    # L1DLM = re.compile(r'\s*(\d+)\s*(L1-dcache-load-misses).*')
+    # # L1-icache-load-misses
+    # L1ILM = re.compile(r'\s*(\d+)\s*(L1-icache-load-misses).*')
+    # # Cache-Reference
+    # CR = re.compile(r'\s*(\d+)\s*(cache-references).*')
+    # # Bus-Cycle
+    # BC = re.compile(r'\s*(\d+)\s*(bus-cycles).*')
+    # # L1-dcache-loads
+    # L1DL = re.compile(r'\s*(\d+)\s*(L1-dcache-loads).*')
+    # # L1-icache-loads
+    # L1IL = re.compile(r'\s*(\d+)\s*(L1-icache-loads).*')
+    # # L1-dcache-store-misses
+    # L1DSM = re.compile(r'\s*(\d+)\s*(L1-dcache-store-misses).*')
+    # # bus-access, but only for ARM-A72
+    # BA = re.compile(r'\s*(\d+)\s*(armv8_cortex_a72/bus_access/).*')
     
-    res = {
-        'cache-misses': (CM, WORST_CM),
-        'L1-dcache-load-misses': (L1DLM, WORST_L1DLM),
-        'L1-icache-load-misses': (L1ILM, WORST_L1ILM),
-        'cache-references': (CR, WORST_CR),
-        'bus-cycles': (BC, WORST_BC),
-        'L1-dcache-loads': (L1DL, WORST_L1DL),
-        'L1-icache-loads': (L1IL, WORST_L1IL),
-        'L1-dcache-store-misses': (L1DSM, WORST_L1DSM),
-        'bus-access': (BA, WORST_BA),
-    }
+    # res = {
+    #     'cache-misses': (CM, int()),
+    #     'L1-dcache-load-misses': (L1DLM, int()),
+    #     'L1-icache-load-misses': (L1ILM, int()),
+    #     'cache-references': (CR, int()),
+    #     'bus-cycles': (BC, int()),
+    #     'L1-dcache-loads': (L1DL, int()),
+    #     'L1-icache-loads': (L1IL, int()),
+    #     'L1-dcache-store-misses': (L1DSM, int()),
+    #     'bus-access': (BA, int()),
+    # }
+    
+    init = True
     
     for i in range(repeat):
         p = subprocess.Popen(
@@ -94,18 +89,19 @@ def main() -> int:
             # perf stat -e cache-misses -e L1-dcache-load-misses -e L1-icache-load-misses -e cache-references -e bus-cycles -e bus-access
             # -e L1-dcache-loads -e L1-icache-loads -- ./driver 1 test/CoreInfo.json output.json
             [
-                'perf', 'stat', 
-                '-e', 'cache-misses', 
-                '-e', 'L1-dcache-load-misses', 
-                '-e', 'L1-icache-load-misses', 
-                '-e', 'cache-references', 
-                '-e', 'bus-cycles', 
-                '-e', 'L1-dcache-store-misses', 
-                '-e', 'L1-dcache-loads', 
-                '-e', 'L1-icache-loads', 
-                '-e', 'armv8_cortex_a72/bus_access/', 
-                '--', 
-                './driver', '1', sys.argv[2], sys.argv[3],
+                # Uncomment this if you need to use perf, else keep it
+                # 'perf', 'stat', 
+                # '-e', 'cache-misses', 
+                # '-e', 'L1-dcache-load-misses', 
+                # '-e', 'L1-icache-load-misses', 
+                # '-e', 'cache-references', 
+                # '-e', 'bus-cycles', 
+                # '-e', 'L1-dcache-store-misses', 
+                # '-e', 'L1-dcache-loads', 
+                # '-e', 'L1-icache-loads', 
+                # '-e', 'armv8_cortex_a72/bus_access/', 
+                # '--', 
+                './driver', '1', sys.argv[2], filename,
             ],
             env = {'LD_LIBRARY_PATH': '.'},
             stderr = subprocess.PIPE
@@ -121,25 +117,37 @@ def main() -> int:
             print(f'STDERR: {p.stderr.read().decode("utf-8")}')
             return ret
 
-        outputs = p.stderr.read().decode('utf-8').split('\n')
+        # Same as above
+        # outputs = p.stderr.read().decode('utf-8').split('\n')
         
-        for line in outputs:
-            # Do some regex
-            for key in res:
-                m = res[key][0].match(line)
-                if m is not None:
-                    result = int(m.group(1))
-                    if result > res[key][1]:
-                        pt = res[key][0]
-                        res[key] = (pt, result)
+        # for line in outputs:
+        #     if DEBUG:
+        #         print(f'STDERR: {line}')
+        #     # Do some regex
+        #     for key in res:
+        #         m = res[key][0].match(line)
+        #         if m is not None:
+        #             if DEBUG:
+        #                 print(f'Line: {line}')
+        #                 print(f'Match: {m.group(0)}')
+        #                 print(f'Val: {m.group(1)}')
+        #             result = int(m.group(1))
+        #             if init:
+        #                 init = False
+        #                 pt = res[key][0]
+        #                 res[key] = (pt, result)
+        #             else:
+        #                 if result > res[key][1]:
+        #                     pt = res[key][0]
+        #                     res[key] = (pt, result)
         
         base = process_json(filename, base, i)
     
     with open(sys.argv[3], 'w') as f:
         json.dump(base, f, indent = 4)
         
-    for key in res:
-        print(f'Worst {key}: {res[key][1]}')
+    # for key in res:
+    #     print(f'Worst {key}: {res[key][1]}')
     
     return 0
 
